ffc161949894afee2ffbe022ef46e0d99b19dd05
Dirk Lemstra <dirk@lemstra.org>
2020-02-16 17:48:24 +0100

    Load the image in the before each.

--- tests/magick-image/resize.spec.ts
+++ tests/magick-image/resize.spec.ts
@@ -7,6 +7,7 @@ let image: MagickImage;
 beforeEach(() => {
     ImageMagick._api = (global as any).native;
     image = new MagickImage();
+    image.read('logo:');
 });
 
 afterEach(() => {
@@ -15,28 +16,24 @@ afterEach(() => {
 
 describe('MagickImage#resize', () => {
     it('should change the width of the image', () => {
-        image.read('logo:');
         image.resize(400, 0);
         expect(image.width).toEqual(400);
         expect(image.height).toEqual(300);
     });
 
     it('should change the height of the image', () => {
-        image.read('logo:');
         image.resize(0, 400);
         expect(image.width).toEqual(533);
         expect(image.height).toEqual(400);
     });
 
     it('with geometry should change the width of the image', () => {
-        image.read('logo:');
         image.resize(new MagickGeometry(300, 0));
         expect(image.width).toEqual(300);
         expect(image.height).toEqual(225);
     });
 
     it('with geometry should change the height of the image', () => {
-        image.read('logo:');
         image.resize(new MagickGeometry(0, 300));
         expect(image.width).toEqual(400);
         expect(image.height).toEqual(300);
