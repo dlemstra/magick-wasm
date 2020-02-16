83824848427868d1d547a9e9c7443c4b392087ab
Dirk Lemstra <dirk@lemstra.org>
2020-02-16 15:32:45 +0100

    Added getArea to the pixel collection.

--- src/pixels/pixel-collection.ts
+++ src/pixels/pixel-collection.ts
@@ -3,25 +3,30 @@ import { Exception } from "../exception/exception";
 import { withString } from "../util/string";
 import { NativeInstance } from "../native-instance";
 import { MagickImage } from "../magick-image";
+import { quantumArray } from "../wasm/magick";
 
 export class PixelCollection extends NativeInstance {
-    private constructor(image: number) {
+    private readonly image: MagickImage;
+
+    private constructor(image: MagickImage) {
         const instance = Exception.usePointer((exception) => {
-            return ImageMagick._api._PixelCollection_Create(image, exception);
+            return ImageMagick._api._PixelCollection_Create(image._instance, exception);
         });
         const disposeMethod = ImageMagick._api._PixelCollection_Dispose;
 
         super(instance, disposeMethod);
+
+        this.image = image;
     }
 
     /** @internal */
     static _create(image: MagickImage): PixelCollection {
-        return new PixelCollection(image._instance);
+        return new PixelCollection(image);
     }
 
     /** @internal */
     static _use<TReturnType>(image: MagickImage, func: (pixels: PixelCollection) => TReturnType): TReturnType {
-        const pixels = new PixelCollection(image._instance);
+        const pixels = new PixelCollection(image);
         try {
             return func(pixels);
         } finally {
@@ -31,7 +36,7 @@ export class PixelCollection extends NativeInstance {
 
     /** @internal */
     static _map(image: MagickImage, mapping: string, func: (instance: number) => void): void {
-        const pixels = new PixelCollection(image._instance);
+        const pixels = new PixelCollection(image);
         try {
             pixels.use(0, 0, image.width, image.height, mapping, (instance) => {
                 func(instance);
@@ -42,13 +47,21 @@ export class PixelCollection extends NativeInstance {
         }
     }
 
+    getArea(x: number, y: number, width: number, height: number): quantumArray {
+        return Exception.usePointer((exception) => {
+            const instance = ImageMagick._api._PixelCollection_GetArea(this._instance, x, y, width, height, exception);
+            const count = width * height * this.image.channelCount;
+            return ImageMagick._api.HEAPU8.subarray(instance, instance + count);
+        });
+    }
+
     toByteArray(x: number, y: number, width: number, height: number, mapping: string): Uint8Array | null {
         return this.use(x, y, width, height, mapping, (instance) => {
             return PixelCollection.createArray(instance, width, height, mapping.length);
         });
     }
 
-    static createArray(instance: number, width: number, height: number, channelCount: number): Uint8Array | null {
+    private static createArray(instance: number, width: number, height: number, channelCount: number): Uint8Array | null {
         if (instance === 0)
             return null;
 
---
+++ tests/pixels/pixel-collection/get-area.spec.ts
@@ -0,0 +1,26 @@
+import { ImageMagick } from '../../../src/image-magick';
+import { MagickImage } from '../../../src/magick-image';
+import { PixelCollection } from '../../../src/pixels/pixel-collection';
+
+let image: MagickImage;
+let pixels: PixelCollection;
+
+beforeEach(() => {
+    ImageMagick._api = (global as any).native;
+    image = new MagickImage();
+    image.read('logo:');
+    pixels = PixelCollection._create(image);
+});
+
+afterEach(() => {
+    image.dispose();
+    pixels.dispose();
+});
+
+describe('PixelCollection#toByteArray', () => {
+    it('should return array with the correct size', () => {
+        const data = pixels.getArea(0, 0, 4, 5);
+        expect(data).not.toBeNull();
+        expect(data!.length).toEqual(80);
+    });
+});
