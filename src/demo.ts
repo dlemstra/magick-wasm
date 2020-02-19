d6ca7e6a8ea56ce5c66729f5b28fb5db22c3bf53
Dirk Lemstra <dirk@lemstra.org>
2020-02-19 07:58:49 +0100

    Removed log events due to issues with function pointers after enabling EMULATE_FUNCTION_POINTER_CASTS.

--- src/demo.ts
+++ src/demo.ts
@@ -1,14 +1,8 @@
 import { initializeImageMagick, ImageMagick } from "./image-magick";
-import { LogEvents } from "./log-events";
 import { Magick } from "./magick";
 import { Quantum } from "./quantum";
 
 initializeImageMagick().then(async () => {
-    const messages: string[] = [];
-    Magick.logEvents(LogEvents.Trace | LogEvents.Coder, (type, message) => {
-        messages.push(message);
-    });
-
     console.log(Magick.imageMagickVersion);
     console.log('Delegates:', Magick.delegates);
     console.log('Features:', Magick.features);
@@ -18,8 +12,6 @@ initializeImageMagick().then(async () => {
     ImageMagick.read('logo:', (image) => {
         console.log(image.toString());
     });
-
-    console.log(messages.length, messages.pop());
 
     ImageMagick.read('foobar:', () => { });
 }).catch((err) => {
--- src/magick.ts
+++ src/magick.ts
@@ -1,13 +1,6 @@
 import { ImageMagick } from "./image-magick";
-import { LogEvents } from "./log-events";
-import { withString } from "./util/string";
-
-export type logDelegate = (type: LogEvents, message: string) => void;
 
 export class Magick {
-    private static LogDelegate = 0;
-    private static LogDelegates: logDelegate[] = [];
-
     static get delegates(): string { return ImageMagick._api.UTF8ToString(ImageMagick._api._Magick_Delegates_Get()); }
 
     static get features(): string { return ImageMagick._api.UTF8ToString(ImageMagick._api._Magick_Features_Get()).slice(0, -1); }
@@ -15,40 +8,4 @@ export class Magick {
     static get imageMagickVersion(): string { return ImageMagick._api.UTF8ToString(ImageMagick._api._Magick_ImageMagickVersion_Get()); }
 
     static setRandomSeed = (seed: number): void => ImageMagick._api._Magick_SetRandomSeed(seed);
-
-    static logEvents(logEvents: LogEvents, func: logDelegate): void {
-        Magick.LogDelegates.push(func);
-        if (Magick.LogDelegate === 0) {
-            Magick.LogDelegate = ImageMagick._api.addFunction((type: number, ptr: number) => Magick.onLog(type, ptr), 'vii');
-            ImageMagick._api._Magick_SetLogDelegate(Magick.LogDelegate);
-
-            const eventNames = Magick.getEventNames(logEvents);
-            withString(eventNames, (events: number) => {
-                ImageMagick._api._Magick_SetLogEvents(events);
-            });
-        }
-    }
-
-    private static onLog(type: number, ptr: number): void {
-        const message = ImageMagick._api.UTF8ToString(ptr);
-        Magick.LogDelegates.forEach((delegate) => {
-            delegate(type, message);
-        });
-    }
-
-    private static getEventNames(logEvents: LogEvents): string {
-        if (logEvents === LogEvents.All)
-            return 'All,Trace'
-
-        if (logEvents === LogEvents.Detailed)
-            return 'All'
-
-        const values = [];
-        for (const value of Object.keys(LogEvents).map(item => Number(item)).filter(item => !isNaN(item) && item > 0)) {
-            if ((value & logEvents) === value)
-                values.push(LogEvents[value]);
-        }
-
-        return values.join(',');
-    }
 }
