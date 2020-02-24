073ba28350ca0e219d1d2fa99f7249ed072ce9dd
Dirk Lemstra <dirk@lemstra.org>
2020-02-24 23:42:09 +0100

    Added supportedFormats property to the Magick class.

--- src/magick.ts
+++ src/magick.ts
@@ -1,4 +1,5 @@
 import { ImageMagick } from "./image-magick";
+import { MagickFormatInfo } from "./magick-format-info";
 
 export class Magick {
     static get delegates(): string { return ImageMagick._api.UTF8ToString(ImageMagick._api._Magick_Delegates_Get()); }
@@ -6,6 +7,8 @@ export class Magick {
     static get features(): string { return ImageMagick._api.UTF8ToString(ImageMagick._api._Magick_Features_Get()).slice(0, -1); }
 
     static get imageMagickVersion(): string { return ImageMagick._api.UTF8ToString(ImageMagick._api._Magick_ImageMagickVersion_Get()); }
+
+    static get supportedFormats(): MagickFormatInfo[] { return MagickFormatInfo.all; }
 
     static setRandomSeed = (seed: number): void => ImageMagick._api._Magick_SetRandomSeed(seed);
 }
---
+++ tests/magick/supportedFormats.spec.ts
@@ -0,0 +1,13 @@
+import { ImageMagick } from '../../src/image-magick';
+import { Magick } from '../../src/magick';
+
+beforeAll(() => { ImageMagick._api = (global as any).native; });
+
+describe('Magick#supportedFormats', () => {
+    it('should return the supported formats', () => {
+        const formats = Magick.supportedFormats;
+
+        expect(formats).not.toBeNull();
+        expect(formats.length).toEqual(245);
+    });
+});
