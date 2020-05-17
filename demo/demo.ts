5bad7cf04edf10504e70db6d936b59cc202ba150
Dirk Lemstra <dirk@lemstra.org>
2020-05-17 20:47:55 +0200

    Added more examples to the demo project.

--- demo/demo.ts
+++ demo/demo.ts
@@ -2,6 +2,7 @@
 
 import { initializeImageMagick, ImageMagick } from "../lib/image-magick";
 import { Magick } from "../lib/magick";
+import { MagickFormat } from "../lib/magick-format";
 import { Quantum } from "../lib/quantum";
 
 initializeImageMagick().then(async () => {
@@ -12,11 +13,14 @@ initializeImageMagick().then(async () => {
 
     console.log('');
     ImageMagick.read('logo:', (image) => {
+        image.resize(100, 100);
+        image.blur(1, 5);
         console.log(image.toString());
-    });
 
-    ImageMagick.read('foobar:', (image) => {
-        console.log(image.toString());
+        image.write((data) =>
+        {
+            console.log(data.length);
+        }, MagickFormat.Jpeg);
     });
 }).catch((err) => {
     console.error(err);
