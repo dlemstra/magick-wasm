f7d093770d54869308fc0732066a318e608ef9c4
Dirk Lemstra <dirk@lemstra.org>
2020-08-09 15:40:24 +0200

    Added Gravity enum.

---
+++ src/gravity.ts
@@ -0,0 +1,15 @@
+/* Copyright Dirk Lemstra https://github.com/dlemstra/Magick.WASM */
+
+export enum Gravity {
+    Undefined,
+    Forget = 0,
+    Northwest = 1,
+    North = 2,
+    Northeast = 3,
+    West = 4,
+    Center = 5,
+    East = 6,
+    Southwest = 7,
+    South = 8,
+    Southeast = 9,
+}
