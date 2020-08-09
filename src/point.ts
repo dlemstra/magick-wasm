ac428317aa527eb6ce9d855c2207c98252f27620
Dirk Lemstra <dirk@lemstra.org>
2020-08-09 15:40:37 +0200

    Added Point class.

---
+++ src/point.ts
@@ -0,0 +1,19 @@
+/* Copyright Dirk Lemstra https://github.com/dlemstra/Magick.WASM */
+
+export class Point {
+    private _x: number;
+    private _y: number;
+
+    constructor(xy: number);
+    constructor(x: number, y: number);
+    constructor(x: number, y?: number) {
+        this._x = x;
+        this._y = y ?? x;
+    }
+
+    get x(): number { return this._x; }
+    set x(value: number) { this._x = value; }
+
+    get y(): number { return this._y; }
+    set y(value: number) { this._y = value; }
+}
