cec825dab394d12f847db8c586421bd5815c9a18
Dirk Lemstra <dirk@lemstra.org>
2021-03-06 14:42:55 +0100

    Some more code style changes.

--- src/formats/dng/dng-read-defines.ts
+++ src/formats/dng/dng-read-defines.ts
@@ -4,37 +4,37 @@ import { IDefine } from '../../defines/define';
 import { MagickFormat } from '../../magick-format';
 
 export class DngReadDefines extends DefinesCreator {
-  constructor () {
-    super(MagickFormat.Dng);
-  }
+    constructor () {
+      super(MagickFormat.Dng);
+    }
 
-  disableAutoBrightness?: boolean;
+    disableAutoBrightness?: boolean;
 
-  outputColor?: DngOutputColor;
+    outputColor?: DngOutputColor;
 
-  useAutoWhitebalance?: boolean;
+    useAutoWhitebalance?: boolean;
 
-  useCameraWhitebalance?: boolean;
+    useCameraWhitebalance?: boolean;
 
-  getDefines(): IDefine[] {
-    const defines: IDefine[] = [];
+    getDefines(): IDefine[] {
+        const defines: IDefine[] = [];
 
-    if (this.hasValue(this.disableAutoBrightness))
-      defines.push(this.createDefine('no-auto-bright', this.disableAutoBrightness as boolean));
+        if (this.hasValue(this.disableAutoBrightness))
+            defines.push(this.createDefine('no-auto-bright', this.disableAutoBrightness as boolean));
 
-    if (this.hasValue(this.outputColor))
-      defines.push(this.createDefine('output-color', this.outputColor as number));
+        if (this.hasValue(this.outputColor))
+            defines.push(this.createDefine('output-color', this.outputColor as number));
 
-    if (this.hasValue(this.useCameraWhitebalance))
-      defines.push(this.createDefine('use-camera-wb', this.useCameraWhitebalance as boolean));
+        if (this.hasValue(this.useCameraWhitebalance))
+            defines.push(this.createDefine('use-camera-wb', this.useCameraWhitebalance as boolean));
 
-    if (this.hasValue(this.useAutoWhitebalance))
-      defines.push(this.createDefine('use-auto-wb', this.useAutoWhitebalance as boolean));
+        if (this.hasValue(this.useAutoWhitebalance))
+            defines.push(this.createDefine('use-auto-wb', this.useAutoWhitebalance as boolean));
 
-    return defines;
-  }
+        return defines;
+    }
 
-  private hasValue(value: boolean | DngOutputColor | undefined): boolean {
-    return value !== undefined && value !== null;
-  }
+    private hasValue(value: boolean | DngOutputColor | undefined): boolean {
+        return value !== undefined && value !== null;
+    }
 }
