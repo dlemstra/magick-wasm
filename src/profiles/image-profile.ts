16c1a89b3405f7f9c9dbb4f2eed405a64b04f9c1
Dirk Lemstra <dirk@lemstra.org>
2021-03-06 14:02:24 +0100

    Mark field as readonly.

--- src/profiles/image-profile.ts
+++ src/profiles/image-profile.ts
@@ -2,15 +2,14 @@
 // Licensed under the Apache License, Version 2.0.
 
 export class ImageProfile {
-    private _name: string;
     private _data: Uint8Array;
 
     constructor(name: string, data: Uint8Array) {
-        this._name = name;
+        this.name = name;
         this._data = data;
     }
 
-    get name(): string { return this._name; }
+    readonly name: string;
 
     getData(): Uint8Array {
         return this._data;
