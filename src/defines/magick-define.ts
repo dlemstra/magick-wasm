4790cddfeebb483e908c34b9bc9bb7f92f97c22d
Michał Zalewski <1094593+mizalewski@users.noreply.github.com>
2021-03-06 14:15:03 +0100

    Change fields in IDefine and MagickDefine to readonly

--- src/defines/magick-define.ts
+++ src/defines/magick-define.ts
@@ -2,9 +2,9 @@ import {IDefine} from './define';
 import {MagickFormat} from '../magick-format';
 
 export class MagickDefine implements IDefine {
-  format: MagickFormat;
-  name: string;
-  value: string;
+  readonly format: MagickFormat;
+  readonly name: string;
+  readonly value: string;
 
   constructor (format: MagickFormat, name: string, value: string) {
     this.format = format;
