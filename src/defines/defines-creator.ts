ad85fff17a502cee56e1776d8517790444451bb9
Michał Zalewski <1094593+mizalewski@users.noreply.github.com>
2021-03-06 12:04:42 +0100

    Remove createDefine for array value in DefinesCreator class

--- src/defines/defines-creator.ts
+++ src/defines/defines-creator.ts
@@ -3,10 +3,6 @@ import {IDefine} from './define';
 import {MagickFormat} from '../magick-format';
 import {MagickDefine} from './magick-define';
 
-interface IHasToString {
-  toString(): string
-}
-
 export abstract class DefinesCreator implements IDefines {
   format: MagickFormat;
 
@@ -17,36 +13,15 @@ export abstract class DefinesCreator implements IDefines {
   createDefine(name: string, value: boolean): MagickDefine;
   createDefine(name: string, value: number): MagickDefine;
   createDefine(name: string, value: string): MagickDefine;
-  createDefine(name: string, value: unknown[]): MagickDefine | null;
-  createDefine(name: string, value: boolean | number | string | unknown[]): MagickDefine | null {
+  createDefine(name: string, value: boolean | number | string): MagickDefine {
     if (typeof value === 'boolean') {
       return new MagickDefine(this.format, name, value ? 'true' : 'false');
     } else if (typeof value === 'string') {
       return new MagickDefine(this.format, name, value);
-    } else if (typeof value === 'number') {
-      return new MagickDefine(this.format, name, value.toString());
     } else {
-      if (value === null) {
-        return null;
-      }
-
-      const values = value.map(v => {
-        if (this.hasToString(v)) {
-          return v.toString();
-        }
-      });
-      if (values.length === 0) {
-        return null;
-      }
-
-      return new MagickDefine(this.format, name, values.join(','));
+      return new MagickDefine(this.format, name, value.toString());
     }
   }
 
   abstract getDefines (): IDefine[]
-
-  private hasToString(obj: unknown): obj is IHasToString {
-    return (obj as IHasToString).toString !== undefined
-      && typeof (obj as IHasToString).toString === 'function';
-  }
 }
