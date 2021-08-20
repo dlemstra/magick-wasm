88d0339dc223b0a376aa8d4cf6060ec5993b23a5
Dirk Lemstra <dirk@lemstra.org>
2021-08-20 12:54:59 +0200

    Use interface instead.

--- tests/save-image.ts
+++ tests/save-image.ts
@@ -1,8 +1,8 @@
-import { MagickImage } from '../src/magick-image';
+import { IMagickImage } from '../src/magick-image';
 import { MagickFormat } from '../src/magick-format';
 import * as fs from 'fs';
 
-export function saveImage(image: MagickImage, fileName: string): void {
+export function saveImage(image: IMagickImage, fileName: string): void {
     const info = fileName.split('.');
     const format = info[info.length - 1].toUpperCase() as MagickFormat;
     image.write(data => {
