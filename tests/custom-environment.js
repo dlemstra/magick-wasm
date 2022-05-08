74edb866e9a6ddd54c9c155cf718220782b816a0
Dirk Lemstra <dirk@lemstra.org>
2022-05-08 07:59:36 +0200

    Require now needs .default.

--- tests/custom-environment.js
+++ tests/custom-environment.js
@@ -1,7 +1,7 @@
 // Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
-const NodeEnvironment = require('jest-environment-node');
+const NodeEnvironment = require('jest-environment-node').default;
 const ImageMagick = require('../lib/image-magick.js');
 
 let native = undefined;
