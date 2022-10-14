6ab5b882b852274e43426ac6277a40f4d803966d
Dirk Lemstra <dirk@lemstra.org>
2022-10-14 16:43:06 +0200

    Added eslint plugin that can be used to check the header of the files.

--- tests/save-image.ts
+++ tests/save-image.ts
@@ -1,3 +1,6 @@
+// Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
+// Licensed under the Apache License, Version 2.0.
+
 import { IMagickImage } from '../src/magick-image';
 import { MagickFormat } from '../src/magick-format';
 import * as fs from 'fs';
