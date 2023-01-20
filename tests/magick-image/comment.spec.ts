d931c88123e8ddbf57ee42e7ad3b6879bc2d6f0a
Dirk Lemstra <dirk@lemstra.org>
2023-01-20 23:04:41 +0100

    Added unit test for comment.

---
+++ tests/magick-image/comment.spec.ts
@@ -0,0 +1,29 @@
+// Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
+// Licensed under the Apache License, Version 2.0.
+
+import { ImageMagick } from '../../src/image-magick';
+import { IMagickImage, MagickImage } from '../../src/magick-image';
+
+let image: IMagickImage;
+
+beforeEach(() => {
+    ImageMagick._api = global.native;
+    image = MagickImage.create();
+    image.read('logo:');
+});
+
+afterEach(() => {
+    image.dispose();
+});
+
+describe('MagickImage#comment', () => {
+    it('should have null as default value', () => {
+        expect(image.comment).toBeNull();
+    });
+
+    it('should set the comment attribute', () => {
+        image.comment = "foo";
+        expect(image.comment).toBe("foo");
+        expect(image.getAttribute("comment")).toBe("foo");
+    });
+});
