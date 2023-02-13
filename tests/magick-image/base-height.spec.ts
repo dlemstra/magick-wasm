2801d57010c30538edb27df3bb0aa258e6dd42ac
Dirk Lemstra <dirk@lemstra.org>
2023-02-13 21:02:51 +0100

    Correct initialization.

--- tests/magick-image/base-height.spec.ts
+++ tests/magick-image/base-height.spec.ts
@@ -1,7 +1,12 @@
 // Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
+import { ImageMagick } from '../../src/image-magick';
 import { TestFiles } from '../test-files';
+
+beforeEach(() => {
+    ImageMagick._api = global.native;
+});
 
 describe('MagickImage#baseHeight', () => {
     it('should return the height of the image before transformations.', async () => {
