1702581551845978df275868b52a8ff946c2d907
Dirk Lemstra <dirk@lemstra.org>
2023-04-18 23:32:21 +0200

    Added unit test for colorSpace.

---
+++ tests/magick-image/color-space.spec.ts
@@ -0,0 +1,23 @@
+// Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
+// Licensed under the Apache License, Version 2.0.
+
+import { ColorSpace } from '../../src/color-space';
+import { ImageMagick } from '../../src/image-magick';
+import { TestFiles } from '../test-files';
+
+beforeAll(() => { ImageMagick._api = global.native; });
+
+describe('MagickImage#colorSpace', () => {
+    it('should return the color space', () => {
+        TestFiles.Builtin.logo.read(image => {
+            expect(image.colorSpace).toBe(ColorSpace.sRGB);
+        });
+    });
+
+    it('should change the color space', () => {
+        TestFiles.Builtin.logo.read(image => {
+            image.colorSpace = ColorSpace.CMYK;
+            expect(image.colorSpace).toBe(ColorSpace.CMYK);
+        });
+    });
+});
