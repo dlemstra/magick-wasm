3ea0837612fc3136cf16bfe6a71e5fd83ed1086c
Dirk Lemstra <dirk@lemstra.org>
2023-05-13 11:57:55 +0200

    Moved variable to the correct line.

--- src/settings/native-magick-settings.ts
+++ src/settings/native-magick-settings.ts
@@ -20,9 +20,6 @@ export class NativeMagickSettings extends NativeInstance {
             });
         }
 
-        if (settings.interlace !== undefined)
-            ImageMagick._api._MagickSettings_Interlace_Set(this._instance, settings.interlace);
-
         if (settings._ping)
             ImageMagick._api._MagickSettings_SetPing(this._instance, 1);
 
@@ -60,6 +57,9 @@ export class NativeMagickSettings extends NativeInstance {
                 ImageMagick._api._MagickSettings_Format_Set(this._instance, ptr);
             });
         }
+
+        if (settings.interlace !== undefined)
+            ImageMagick._api._MagickSettings_Interlace_Set(this._instance, settings.interlace);
 
         if (settings.strokeColor !== undefined)
             this.setOption('stroke', settings.strokeColor.toString());
