b317c060470c822c17b557cf71624d587201686e
Rubens Cordeiro <zyoncore@hotmail.com>
2023-05-22 14:20:21 -0300

    Removed the empty lines from tests/remove-profile

--- tests/magick-image/remove-profile.spec.ts
+++ tests/magick-image/remove-profile.spec.ts
@@ -5,18 +5,12 @@ import { TestImages } from '../test-images';
 
 describe('MagickImage#removeProfile', () => {
     it('should remove the profile from the image', () => {
-
         TestImages.fujiFilmFinePixS1ProJpg.use(image => {
-
             let profile = image.getProfile('icc');
             expect(profile).not.toBeNull();
-
             if (profile !== null) {
-
                 expect(profile.name).toEqual('icc');
-
                 image.removeProfile('icc');
-
                 profile = image.getProfile('icc');
                 expect(profile).toBeNull();
             }
