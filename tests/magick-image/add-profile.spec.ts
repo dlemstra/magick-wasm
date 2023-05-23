49147c05b00da638cdee20fef5ef9aba063bf714
Rubens Cordeiro <zyoncore@hotmail.com>
2023-05-23 10:13:38 -0300

    Fix encoding

--- tests/magick-image/add-profile.spec.ts
+++ tests/magick-image/add-profile.spec.ts
@@ -1,4 +1,4 @@
-﻿// Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
+// Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
 import { IMagickImage, MagickImage } from '../../src/magick-image';
