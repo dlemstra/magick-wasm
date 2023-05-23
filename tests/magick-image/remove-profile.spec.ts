328e949a6a23a368903ed2950e9d353823e22694
Dirk Lemstra <dirk@lemstra.org>
2023-05-23 07:36:01 +0200

    Whitespace cleanup.

--- tests/magick-image/remove-profile.spec.ts
+++ tests/magick-image/remove-profile.spec.ts
@@ -7,11 +7,16 @@ describe('MagickImage#removeProfile', () => {
     it('should remove the profile from the image', () => {
         TestImages.fujiFilmFinePixS1ProJpg.use(image => {
             let profile = image.getProfile('icc');
+
             expect(profile).not.toBeNull();
+
             if (profile !== null) {
                 expect(profile.name).toEqual('icc');
+
                 image.removeProfile('icc');
+
                 profile = image.getProfile('icc');
+
                 expect(profile).toBeNull();
             }
         });
