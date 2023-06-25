419bf62539d31c60ecc8591225a93ee2ad60297e
Rubens Cordeiro <zyoncore@hotmail.com>
2023-06-25 19:02:53 -0300

    added solarize to MagickImage

--- src/magick-image.ts
+++ src/magick-image.ts
@@ -247,6 +247,8 @@ export interface IMagickImage extends IDisposable {
     sigmoidalContrast(contrast: number, midpoint: number): void;
     sigmoidalContrast(contrast: number, midpoint: number, channels: Channels): void;
     splice(geometry: MagickGeometry): void;
+    solarize(): void;
+    solarize(factor: Percentage | number): void;
     statistics(): IStatistics;
     statistics(channels: Channels): IStatistics;
     strip(): void;
@@ -1326,6 +1328,14 @@ export class MagickImage extends NativeInstance implements IMagickImage {
     sigmoidalContrast(contrast: number, midpoint: number, channels: Channels): void;
     sigmoidalContrast(contrast: number, midpointOrPercentage?: number | Percentage, channelsOrUndefined?: Channels): void {
         this._sigmoidalContrast(true, contrast, midpointOrPercentage, channelsOrUndefined)
+    }
+
+    solarize(): void
+    solarize(factorOrNumber: Percentage | number = new Percentage(50)): void {
+        Exception.use(exception => {
+            const factor = typeof factorOrNumber === "number" ? new Percentage(factorOrNumber) : factorOrNumber;
+            ImageMagick._api._MagickImage_Solarize(this._instance, factor.toQuantum(), exception.ptr);
+        });
     }
 
     splice(geometry: MagickGeometry): void {
