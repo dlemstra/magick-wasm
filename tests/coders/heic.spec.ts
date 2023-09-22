e00426111836b97e9397befb74ad09fc21bf1fbb
Dirk Lemstra <dirk@lemstra.org>
2023-09-22 07:24:26 +0200

    Also read the created image.

--- tests/coders/heic.spec.ts
+++ tests/coders/heic.spec.ts
@@ -1,6 +1,7 @@
 // Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
+import { ImageMagick } from '../../src/image-magick';
 import { MagickFormat } from '../../src/magick-format';
 import { TestImages } from '../test-images';
 
@@ -9,6 +10,9 @@ describe('Coders#heic', () => {
         TestImages.Builtin.logo.use((image) => {
             image.write(MagickFormat.Avif, data => {
                 expect(data.length).toBe(11386);
+                ImageMagick.read(data, image => {
+                    expect(image.format).toBe(MagickFormat.Avif);
+                });
             });
         });
     });
