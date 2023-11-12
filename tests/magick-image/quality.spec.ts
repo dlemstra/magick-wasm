e7f629ad2fa40ba10ec10cbd2a6e52ea6b804a43
Dirk Lemstra <dirk@lemstra.org>
2023-11-12 14:52:13 +0100

    Switch to using empty test image.

--- tests/magick-image/quality.spec.ts
+++ tests/magick-image/quality.spec.ts
@@ -1,28 +1,21 @@
 // Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
-import { IMagickImage, MagickImage } from '@src/magick-image';
 import { TestImages } from '@test/test-images';
-
-let image: IMagickImage;
-
-beforeEach(() => {
-    image = MagickImage.create();
-});
-
-afterEach(() => {
-    image.dispose();
-});
 
 describe('MagickImage#quality', () => {
     it('should not allow a value below 1', () => {
-        image.quality = 0;
-        expect(image.quality).toBe(1);
+        TestImages.empty.use(image => {
+            image.quality = 0;
+            expect(image.quality).toBe(1);
+        });
     });
 
     it('should not allow a value above 100', () => {
-        image.quality = 101;
-        expect(image.quality).toBe(100);
+        TestImages.empty.use(image => {
+            image.quality = 101;
+            expect(image.quality).toBe(100);
+        });
     });
 
     it('should return the image quality', async () => {
