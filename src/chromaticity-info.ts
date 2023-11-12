b53ec908c16a789b02e9f4c17f5b839be450b309
Dirk Lemstra <dirk@lemstra.org>
2023-11-12 17:43:38 +0100

    Added docs to ChromaticityInfo.

--- src/chromaticity-info.ts
+++ src/chromaticity-info.ts
@@ -3,16 +3,34 @@
 
 import { PrimaryInfo } from './primary-info';
 
+/**
+ * Class that contains information about the chromaticity.
+ */
 export class ChromaticityInfo {
-    red: PrimaryInfo;
-    green: PrimaryInfo;
-    blue: PrimaryInfo;
-    white: PrimaryInfo;
-
     constructor(red: PrimaryInfo, green: PrimaryInfo, blue: PrimaryInfo, white: PrimaryInfo) {
         this.red = red;
         this.green = green;
         this.blue = blue;
         this.white = white;
     }
+
+    /**
+     * Gets or sets the chromaticity red primary point.
+     */
+    red: PrimaryInfo;
+
+    /**
+     * Gets or sets the chromaticity green primary point.
+     */
+    green: PrimaryInfo;
+
+    /**
+     * Gets or sets the chromaticity blue primary point.
+     */
+    blue: PrimaryInfo;
+
+    /**
+     * Gets or sets the chromaticity white primary point.
+     */
+    white: PrimaryInfo;
 }
