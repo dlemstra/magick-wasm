08ae7fa4b3fe8962d47880e7bf5d18240e601eac
Dirk Lemstra <dirk@lemstra.org>
2023-11-18 11:00:25 +0100

    Added docs to the Quantum class.

--- src/quantum.ts
+++ src/quantum.ts
@@ -3,7 +3,17 @@
 
 import { ImageMagick } from './image-magick';
 
+/**
+ * Class that can be used to acquire information about the Quantum.
+ */
 export class Quantum {
+    /**
+     * Gets the quantum depth.
+     */
     static get depth(): number { return ImageMagick._api._Quantum_Depth_Get(); }
+
+    /**
+     * Gets the maximum value of the quantum.
+     */
     static get max(): number { return ImageMagick._api._Quantum_Max_Get(); }
 }
