689377bb4a25b4e5bc099f8c173a4aa87168b21a
Pepijn Peeters <me@pepijnpeeters.nl>
2023-12-18 16:07:36 +0100

    Use single quotes

--- tests/drawables/drawable-text-interword-spacing.spec.ts
+++ tests/drawables/drawable-text-interword-spacing.spec.ts
@@ -1,20 +1,20 @@
 // Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
-import { DrawableFont } from "../../src/drawables/drawable-font";
-import { DrawableFontPointSize } from "../../src/drawables/drawable-font-point-size";
-import { DrawableText } from "../../src/drawables/drawable-text";
-import { DrawableTextInterwordSpacing } from "../../src/drawables/drawable-text-interword-spacing";
-import { TestFonts } from "../test-fonts";
-import { TestImages } from "../test-images";
+import { DrawableFont } from '../../src/drawables/drawable-font';
+import { DrawableFontPointSize } from '../../src/drawables/drawable-font-point-size';
+import { DrawableText } from '../../src/drawables/drawable-text';
+import { DrawableTextInterwordSpacing } from '../../src/drawables/drawable-text-interword-spacing';
+import { TestFonts } from '../test-fonts';
+import { TestImages } from '../test-images';
 
-describe("DrawableTextInterwordSpacing", () => {
-    it("should write text with default interword spacing", () => {
+describe('DrawableTextInterwordSpacing', () => {
+    it('should write text with default interword spacing', () => {
         TestImages.empty150x150Canvas.use((image) => {
             image.draw([
                 new DrawableFont(TestFonts.kaushanScriptRegularTtf.name),
                 new DrawableFontPointSize(50),
-                new DrawableText(50, 50, "I I"),
+                new DrawableText(50, 50, 'I I'),
             ]);
 
             image.trim();
@@ -24,13 +24,13 @@ describe("DrawableTextInterwordSpacing", () => {
         });
     });
 
-    it("should write text with increased interword spacing", () => {
+    it('should write text with increased interword spacing', () => {
         TestImages.empty150x150Canvas.use((image) => {
             image.draw([
                 new DrawableFont(TestFonts.kaushanScriptRegularTtf.name),
                 new DrawableFontPointSize(50),
                 new DrawableTextInterwordSpacing(40),
-                new DrawableText(50, 50, "I I"),
+                new DrawableText(50, 50, 'I I'),
             ]);
 
             image.trim();
@@ -40,13 +40,13 @@ describe("DrawableTextInterwordSpacing", () => {
         });
     });
 
-    it("should write text with negative interword spacing", () => {
+    it('should write text with negative interword spacing', () => {
         TestImages.empty150x150Canvas.use((image) => {
             image.draw([
                 new DrawableFont(TestFonts.kaushanScriptRegularTtf.name),
                 new DrawableFontPointSize(50),
                 new DrawableTextInterwordSpacing(-10),
-                new DrawableText(50, 50, "I I"),
+                new DrawableText(50, 50, 'I I'),
             ]);
 
             image.trim();
