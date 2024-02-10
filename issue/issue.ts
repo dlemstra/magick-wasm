5f3340a321c2a8a1b7f0646640f0cba41b197501
Dirk Lemstra <dirk@lemstra.org>
2024-02-10 21:41:01 +0100

    Also include outputFile in file that is used to test issue.

--- issue/issue.ts
+++ issue/issue.ts
@@ -1,16 +1,20 @@
 // Copyright Dirk Lemstra https://github.com/dlemstra/magick-wasm.
 // Licensed under the Apache License, Version 2.0.
 
-import { readFileSync } from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import { initializeImageMagick, ImageMagick } from '@imagemagick/magick-wasm';
 
 const inputFile = '';
+const outputFile = '';
 const bytes = readFileSync(inputFile);
 
 const wasmLocation = '../node_modules/@dlemstra/magick-native/magick.wasm';
 const wasmBytes = readFileSync(wasmLocation);
 initializeImageMagick(wasmBytes).then(() => {
     ImageMagick.read(bytes, (image) => {
+        image.write(data => {
+            writeFileSync(outputFile, data);
+        });
     });
 }).catch(err => {
     console.error(err);
