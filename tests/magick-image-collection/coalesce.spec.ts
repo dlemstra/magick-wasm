86e2af9e93360e84be1277e5bfa606cff2d2a2c9
Dirk Lemstra <dirk@lemstra.org>
2024-08-05 17:26:21 +0200

    Added missing test for empty collection.

--- tests/magick-image-collection/coalesce.spec.ts
+++ tests/magick-image-collection/coalesce.spec.ts
@@ -5,9 +5,17 @@
 
 import { MagickColor } from '@src/magick-color';
 import { MagickColors } from '@src/magick-colors';
+import { MagickImageCollection } from '@src/magick-image-collection';
 import { TestImages } from '@test/test-images';
 
 describe('MagickImageCollection#coalesce', () => {
+    it('should throw exception when collection is empty', () => {
+        expect(() => {
+            const images = MagickImageCollection.create();
+            images.coalesce();
+        }).toThrowError('operation requires at least one image');
+    });
+
     it('should update the colors for each frame', () => {
         TestImages.roseSparkleGif.use(images => {
             expect(images[1]).toHavePixelWithColor(0, 0, MagickColors.Transparent);
