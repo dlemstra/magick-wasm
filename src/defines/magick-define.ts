c1ae2c147f8cefeff5d25504f9ef120031b455b9
Dirk Lemstra <dirk@lemstra.org>
2024-08-05 08:07:10 +0200

    Added comments to the MagickDefine class.

--- src/defines/magick-define.ts
+++ src/defines/magick-define.ts
@@ -7,15 +7,30 @@ import { IDefine } from './define';
 import { MagickFormat } from '../enums/magick-format';
 
 export class MagickDefine implements IDefine {
+    /**
+     * Initializes a new instance of the {@link MagickDefine} class.
+     * @param format
+     * @param name The name of the define.
+     * @param value The value of the define.
+     */
     constructor(format: MagickFormat, name: string, value: string) {
         this.format = format;
         this.name = name;
         this.value = value;
     }
 
+    /**
+     * Gets the format to set the define for.
+     */
     readonly format: MagickFormat;
 
+    /**
+     * Gets the name of the define.
+     */
     readonly name: string;
 
+    /**
+     * Gets the value of the define.
+     */
     readonly value: string;
 }
