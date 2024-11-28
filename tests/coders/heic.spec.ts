b7b2db5b422846d411947b8a1f29f20dd5da3251
Dirk Lemstra <dirk@lemstra.org>
2024-11-28 08:09:44 +0100

    Corrected unit test due to upgrade of ImageMagick.

--- tests/coders/heic.spec.ts
+++ tests/coders/heic.spec.ts
@@ -11,7 +11,7 @@ describe('Coders#heic', () => {
     it('should be able to write avif image', () => {
         TestImages.Builtin.logo.use((image) => {
             image.write(MagickFormat.Avif, data => {
-                expect(data.length).toBe(11353);
+                expect(data.length).toBe(11333);
                 ImageMagick.read(data, image => {
                     expect(image.format).toBe(MagickFormat.Avif);
                 });
