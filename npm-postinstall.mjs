0b2b7d9eb2d52f569bdde5f786aaea0f9cc9492e
Dirk Lemstra <dirk@lemstra.org>
2025-01-09 22:46:54 +0100

    Renamed variables.

--- npm-postinstall.mjs
+++ npm-postinstall.mjs
@@ -2,16 +2,16 @@ import { copyFileSync, existsSync, mkdirSync } from 'fs';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const filename = fileURLToPath(import.meta.url);
-const folder = dirname(filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 function copyFile(src, dest) {
-    const target = resolve(folder, dest);
+    const target = resolve(__dirname, dest);
     const targetFolder = dirname(target);
     if (!existsSync(targetFolder))
         mkdirSync(targetFolder, { recursive: true });
 
-    copyFileSync(resolve(folder, src), target);
+    copyFileSync(resolve(__dirname, src), target);
     console.log(`Copied ${src} to ${dest}`);
 };
 
