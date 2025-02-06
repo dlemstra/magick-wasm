ccba044c6d9f27fface2d443a323eeb1843e9106
Dirk Lemstra <dirk@lemstra.org>
2025-02-07 00:23:27 +0100

    Changed LogEventTypes enum into a const and type.

--- src/enums/log-event-types.ts
+++ src/enums/log-event-types.ts
@@ -6,114 +6,116 @@
 /**
  * Specifies log event types.
  */
-export enum LogEventTypes {
+export const LogEventTypes = {
     /**
-     * Undefined.
+     * None.
      */
-    None = 0x000000,
+    None: 0x000000,
 
     /**
      * Accelerate.
      */
-    Accelerate = 0x00001,
+    Accelerate: 0x00001,
 
     /**
      * Annotate.
      */
-    Annotate = 0x00002,
+    Annotate: 0x00002,
 
     /**
      * Blob.
      */
-    Blob = 0x00004,
+    Blob: 0x00004,
 
     /**
      * Cache.
      */
-    Cache = 0x00008,
+    Cache: 0x00008,
 
     /**
      * Coder.
      */
-    Coder = 0x00010,
+    Coder: 0x00010,
 
     /**
      * Configure.
      */
-    Configure = 0x00020,
+    Configure: 0x00020,
 
     /**
      * Deprecate.
      */
-    Deprecate = 0x00040,
+    Deprecate: 0x00040,
 
     /**
      * Draw.
      */
-    Draw = 0x00080,
+    Draw: 0x00080,
 
     /**
      * Exception.
      */
-    Exception = 0x00100,
+    Exception: 0x00100,
 
     /**
      * Image.
      */
-    Image = 0x00200,
+    Image: 0x00200,
 
     /**
      * Locale.
      */
-    Locale = 0x00400,
+    Locale: 0x00400,
 
     /**
      * Module.
      */
-    Module = 0x00800,
+    Module: 0x00800,
 
     /**
      * Pixel.
      */
-    Pixel = 0x01000,
+    Pixel: 0x01000,
 
     /**
      * Policy.
      */
-    Policy = 0x02000,
+    Policy: 0x02000,
 
     /**
      * Resource.
      */
-    Resource = 0x04000,
+    Resource: 0x04000,
 
     /**
      * Trace.
      */
-    Trace = 0x08000,
+    Trace: 0x08000,
 
     /**
      * Transform.
      */
-    Transform = 0x10000,
+    Transform: 0x10000,
 
     /**
      * User.
      */
-    User = 0x20000,
+    User: 0x20000,
 
     /**
      * Wand.
      */
-    Wand = 0x40000,
+    Wand: 0x40000,
 
     /**
      * Detailed.
      */
-    Detailed = 0x7fff7fff,
+    Detailed: 0x7fff7fff,
 
     /**
      * All.
      */
-    All = Detailed | Trace,
-}
+    get All() { return this.Detailed | this.Trace; }
+} as const;
+
+export type LogEventTypes = typeof LogEventTypes[keyof typeof LogEventTypes];
