8ae09d1096cbfb3c6f3ebfabf2323d0dff9c19bc
Dirk Lemstra <dirk@lemstra.org>
2025-02-07 00:28:51 +0100

    Changed MagickErrorSeverity enum into a const and type.

--- src/enums/magick-error-severity.ts
+++ src/enums/magick-error-severity.ts
@@ -6,234 +6,236 @@
 /**
  * Specifies the severity of a MagickError.
  */
-export enum MagickErrorSeverity {
+export const MagickErrorSeverity = {
     /**
      * Warning.
      */
-    Warning = 300,
+    Warning: 300,
 
     /**
      * Resource limit warning.
      */
-    ResourceLimitWarning = 300,
+    ResourceLimitWarning: 300,
 
     /**
      * Type warning.
      */
-    TypeWarning = 305,
+    TypeWarning: 305,
 
     /**
      * Option warning.
      */
-    OptionWarning = 310,
+    OptionWarning: 310,
 
     /**
      * Delegate warning.
      */
-    DelegateWarning = 315,
+    DelegateWarning: 315,
 
     /**
      * Missing delegate warning.
      */
-    MissingDelegateWarning = 320,
+    MissingDelegateWarning: 320,
 
     /**
      * Corrupt image warning.
      */
-    CorruptImageWarning = 325,
+    CorruptImageWarning: 325,
 
     /**
      * File open warning.
      */
-    FileOpenWarning = 330,
+    FileOpenWarning: 330,
 
     /**
      * Blob warning.
      */
-    BlobWarning = 335,
+    BlobWarning: 335,
 
     /**
      * Stream warning.
      */
-    StreamWarning = 340,
+    StreamWarning: 340,
 
     /**
      * Cache warning.
      */
-    CacheWarning = 345,
+    CacheWarning: 345,
 
     /**
      * Coder warning.
      */
-    CoderWarning = 350,
+    CoderWarning: 350,
 
     /**
      * Filter warning.
      */
-    FilterWarning = 352,
+    FilterWarning: 352,
 
     /**
      * Module warning.
      */
-    ModuleWarning = 355,
+    ModuleWarning: 355,
 
     /**
      * Draw warning.
      */
-    DrawWarning = 360,
+    DrawWarning: 360,
 
     /**
      * Image warning.
      */
-    ImageWarning = 365,
+    ImageWarning: 365,
 
     /**
      * Wand warning.
      */
-    WandWarning = 370,
+    WandWarning: 370,
 
     /**
      * Random warning.
      */
-    RandomWarning = 375,
+    RandomWarning: 375,
 
     /**
      * X server warning.
      */
-    XServerWarning = 380,
+    XServerWarning: 380,
 
     /**
      * Monitor warning.
      */
-    MonitorWarning = 385,
+    MonitorWarning: 385,
 
     /**
      * Registry warning.
      */
-    RegistryWarning = 390,
+    RegistryWarning: 390,
 
     /**
      * Configure warning.
      */
-    ConfigureWarning = 395,
+    ConfigureWarning: 395,
 
     /**
      * Policy warning.
      */
-    PolicyWarning = 399,
+    PolicyWarning: 399,
 
     /**
      * Error.
      */
-    Error = 400,
+    Error: 400,
 
     /**
      * Resource limit error.
      */
-    ResourceLimitError = 400,
+    ResourceLimitError: 400,
 
     /**
      * Type error.
      */
-    TypeError = 405,
+    TypeError: 405,
 
     /**
      * Option error.
      */
-    OptionError = 410,
+    OptionError: 410,
 
     /**
      * Delegate error.
      */
-    DelegateError = 415,
+    DelegateError: 415,
 
     /**
      * Missing delegate error.
      */
-    MissingDelegateError = 420,
+    MissingDelegateError: 420,
 
     /**
      * Corrupt image error.
      */
-    CorruptImageError = 425,
+    CorruptImageError: 425,
 
     /**
      * File open error.
      */
-    FileOpenError = 430,
+    FileOpenError: 430,
 
     /**
      * Blob error.
      */
-    BlobError = 435,
+    BlobError: 435,
 
     /**
      * Stream error.
      */
-    StreamError = 440,
+    StreamError: 440,
 
     /**
      * Cache error.
      */
-    CacheError = 445,
+    CacheError: 445,
 
     /**
      * Coder error.
      */
-    CoderError = 450,
+    CoderError: 450,
 
     /**
      * Filter error.
      */
-    FilterError = 452,
+    FilterError: 452,
 
     /**
      * Module error.
      */
-    ModuleError = 455,
+    ModuleError: 455,
 
     /**
      * Draw error.
      */
-    DrawError = 460,
+    DrawError: 460,
 
     /**
      * Image error.
      */
-    ImageError = 465,
+    ImageError: 465,
 
     /**
      * Wand error.
      */
-    WandError = 470,
+    WandError: 470,
 
     /**
      * Random error.
      */
-    RandomError = 475,
+    RandomError: 475,
 
     /**
      * X server error.
      */
-    XServerError = 480,
+    XServerError: 480,
 
     /**
      * Monitor error.
      */
-    MonitorError = 485,
+    MonitorError: 485,
 
     /**
      * Registry error.
      */
-    RegistryError = 490,
+    RegistryError: 490,
 
     /**
      * Configure error.
      */
-    ConfigureError = 495,
+    ConfigureError: 495,
 
     /**
      * Policy error.
      */
-    PolicyError = 499
-}
+    PolicyError: 499
+} as const;
+
+export type MagickErrorSeverity = typeof MagickErrorSeverity[keyof typeof MagickErrorSeverity];
