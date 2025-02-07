e8776e006966eafa544b2624b86603c98ae02f54
Dirk Lemstra <dirk@lemstra.org>
2025-02-07 08:34:33 +0100

    Changed PixelChannel enum into a const and type.

--- src/enums/pixel-channel.ts
+++ src/enums/pixel-channel.ts
@@ -6,324 +6,326 @@
 /**
  * Specifies the pixel channels.
  */
-export enum PixelChannel {
+export const PixelChannel = {
     /**
      * Red.
      */
-    Red = 0,
+    Red: 0,
 
     /**
      * Cyan.
      */
-    Cyan = Red,
+    Cyan: 0,
 
     /**
      * Gray.
      */
-    Gray = Red,
+    Gray: 0,
 
     /**
      * Green.
      */
-    Green = 1,
+    Green: 1,
 
     /**
      * Magenta.
      */
-    Magenta = Green,
+    Magenta: 1,
 
     /**
      * Blue.
      */
-    Blue = 2,
+    Blue: 2,
 
     /**
      * Yellow.
      */
-    Yellow = Blue,
+    Yellow: 2,
 
     /**
      * Black.
      */
-    Black = 3,
+    Black: 3,
 
     /**
      * Alpha.
      */
-    Alpha = 4,
+    Alpha: 4,
 
     /**
      * Index.
      */
-    Index = 5,
+    Index: 5,
 
     /**
      * Meta 0.
      */
-    Meta0 = 10,
+    Meta0: 10,
 
     /**
      * Meta 1.
      */
-    Meta1 = 11,
+    Meta1: 11,
 
     /**
      * Meta 2.
      */
-    Meta2 = 12,
+    Meta2: 12,
 
     /**
      * Meta 3.
      */
-    Meta3 = 13,
+    Meta3: 13,
 
     /**
      * Meta 4.
      */
-    Meta4 = 14,
+    Meta4: 14,
 
     /**
      * Meta 5.
      */
-    Meta5 = 15,
+    Meta5: 15,
 
     /**
      * Meta 6.
      */
-    Meta6 = 16,
+    Meta6: 16,
 
     /**
      * Meta 7.
      */
-    Meta7 = 17,
+    Meta7: 17,
 
     /**
      * Meta 8.
      */
-    Meta8 = 18,
+    Meta8: 18,
 
     /**
      * Meta 9.
      */
-    Meta9 = 19,
+    Meta9: 19,
 
     /**
      * Meta 10.
      */
-    Meta10 = 20,
+    Meta10: 20,
 
     /**
      * Meta 11.
      */
-    Meta11 = 21,
+    Meta11: 21,
 
     /**
      * Meta 12.
      */
-    Meta12 = 22,
+    Meta12: 22,
 
     /**
      * Meta 13.
      */
-    Meta13 = 23,
+    Meta13: 23,
 
     /**
      * Meta 14.
      */
-    Meta14 = 24,
+    Meta14: 24,
 
     /**
      * Meta 15.
      */
-    Meta15 = 25,
+    Meta15: 25,
 
     /**
      * Meta 16.
      */
-    Meta16 = 26,
+    Meta16: 26,
 
     /**
      * Meta 17.
      */
-    Meta17 = 27,
+    Meta17: 27,
 
     /**
      * Meta 18.
      */
-    Meta18 = 28,
+    Meta18: 28,
 
     /**
      * Meta 19.
      */
-    Meta19 = 29,
+    Meta19: 29,
 
     /**
      * Meta 20.
      */
-    Meta20 = 30,
+    Meta20: 30,
 
     /**
      * Meta 21.
      */
-    Meta21 = 31,
+    Meta21: 31,
 
     /**
      * Meta 22.
      */
-    Meta22 = 32,
+    Meta22: 32,
 
     /**
      * Meta 23.
      */
-    Meta23 = 33,
+    Meta23: 33,
 
     /**
      * Meta 24.
      */
-    Meta24 = 34,
+    Meta24: 34,
 
     /**
      * Meta 25.
      */
-    Meta25 = 35,
+    Meta25: 35,
 
     /**
      * Meta 26.
      */
-    Meta26 = 36,
+    Meta26: 36,
 
     /**
      * Meta 27.
      */
-    Meta27 = 37,
+    Meta27: 37,
 
     /**
      * Meta 28.
      */
-    Meta28 = 38,
+    Meta28: 38,
 
     /**
      * Meta 29.
      */
-    Meta29 = 39,
+    Meta29: 39,
 
     /**
      * Meta 30.
      */
-    Meta30 = 40,
+    Meta30: 40,
 
     /**
      * Meta 31.
      */
-    Meta31 = 41,
+    Meta31: 41,
 
     /**
      * Meta 32.
      */
-    Meta32 = 42,
+    Meta32: 42,
 
     /**
      * Meta 33.
      */
-    Meta33 = 43,
+    Meta33: 43,
 
     /**
      * Meta 34.
      */
-    Meta34 = 44,
+    Meta34: 44,
 
     /**
      * Meta 35.
      */
-    Meta35 = 45,
+    Meta35: 45,
 
     /**
      * Meta 36.
      */
-    Meta36 = 46,
+    Meta36: 46,
 
     /**
      * Meta 37.
      */
-    Meta37 = 47,
+    Meta37: 47,
 
     /**
      * Meta 38.
      */
-    Meta38 = 48,
+    Meta38: 48,
 
     /**
      * Meta 39.
      */
-    Meta39 = 49,
+    Meta39: 49,
 
     /**
      * Meta 40.
      */
-    Meta40 = 50,
+    Meta40: 50,
 
     /**
      * Meta 41.
      */
-    Meta41 = 51,
+    Meta41: 51,
 
     /**
      * Meta 42.
      */
-    Meta42 = 52,
+    Meta42: 52,
 
     /**
      * Meta 43.
      */
-    Meta43 = 53,
+    Meta43: 53,
 
     /**
      * Meta 44.
      */
-    Meta44 = 54,
+    Meta44: 54,
 
     /**
      * Meta 45.
      */
-    Meta45 = 55,
+    Meta45: 55,
 
     /**
      * Meta 46.
      */
-    Meta46 = 56,
+    Meta46: 56,
 
     /**
      * Meta 47.
      */
-    Meta47 = 57,
+    Meta47: 57,
 
     /**
      * Meta 48.
      */
-    Meta48 = 58,
+    Meta48: 58,
 
     /**
      * Meta 49.
      */
-    Meta49 = 59,
+    Meta49: 59,
 
     /**
      * Meta 50.
      */
-    Meta50 = 60,
+    Meta50: 60,
 
     /**
      * Meta 51.
      */
-    Meta51 = 61,
+    Meta51: 61,
 
     /**
      * Meta 52.
      */
-    Meta52 = 62,
+    Meta52: 62,
 
     /**
      * Composite.
      */
-    Composite = 64,
-}
+    Composite: 64
+} as const;
+
+export type PixelChannel = typeof PixelChannel[keyof typeof PixelChannel];
