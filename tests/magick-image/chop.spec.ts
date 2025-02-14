48460496072e2b3484b5440f9ec6e1ec4c139a53
Dirk Lemstra <dirk@lemstra.org>
2025-02-14 15:43:11 +0100

    Added extra asserts and improved test.

--- tests/magick-image/chop.spec.ts
+++ tests/magick-image/chop.spec.ts
@@ -9,12 +9,14 @@ import { TestFiles } from '@test/test-files';
 describe('MagickImage#chop', () => {
     it('should remove part of the image', () => {
         TestFiles.Images.Builtin.logo.use(image => {
-            image.chop(new MagickGeometry(100, 100, 100, 100));
+            image.chop(new MagickGeometry(75, 100, 150, 25));
 
-            expect(image).toHavePixelWithColor(300, 99, '#fff');
-            expect(image).toHavePixelWithColor(300, 100, '#f79868ff');
-            expect(image).toHavePixelWithColor(406, 99, '#333636ff');
-            expect(image).toHavePixelWithColor(406, 100, '#fff');
+            expect(image.width).toBe(490);
+            expect(image.height).toBe(455);
+            expect(image).toHavePixelWithColor(122, 99, '#fff');
+            expect(image).toHavePixelWithColor(122, 100, '#f5ee36ff');
+            expect(image).toHavePixelWithColor(280, 99, '#fff');
+            expect(image).toHavePixelWithColor(280, 100, '#223e92ff');
         });
     });
 });
