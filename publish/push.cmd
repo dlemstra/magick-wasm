2f1217bd3cfbadc4e8235e2b6f5c35217a316796
Dirk Lemstra <dirk@lemstra.org>
2025-02-23 17:59:21 +0100

    Revert "Published magick-wasm 0.0.33."
    This reverts commit 348f1983876322d5a0311de2f840f3804dad9b0b.

--- publish/push.cmd
+++ publish/push.cmd
@@ -3,7 +3,7 @@
 echo Are you sure?
 pause
 
-set VERSION=0.0.33
+set VERSION=0.0.32
 call npm config set prefix %~dp0
 call npm publish imagemagick-magick-wasm-%VERSION%.tgz --access public
 call npm config set prefix %APPDATA%\npm
