90f9d95d353a025ef7066dff5124b7f237c5b62c
Dirk Lemstra <dirk@lemstra.org>
2025-03-04 08:25:21 +0100

    Corrected linting configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,7 @@ export default [{
         "issue/*.js",
         "tools/*.js",
         "**/dist",
-        "**/vite.config.ts",
+        "**/vite.config.mts",
         "*.mjs",
     ],
 }, ...compat.extends(
