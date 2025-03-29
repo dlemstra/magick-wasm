ad61ab1bdc1c04084593693582fc8d4b108d924f
Dirk Lemstra <dirk@lemstra.org>
2025-03-29 01:36:30 +0100

    Corrected description.

--- tests/magick-image/clone.spec.ts
+++ tests/magick-image/clone.spec.ts
@@ -7,7 +7,7 @@ import { ErrorMetric } from '@src/enums/error-metric';
 import { MagickColors } from '@src/magick-colors';
 import { TestFiles } from '@test/test-files';
 
-describe('MagickImage#channelCount', () => {
+describe('MagickImage#clone', () => {
     it('should create a clone of the image', () => {
         TestFiles.Images.empty.use(image => {
             image.read(MagickColors.Magenta, 1, 1);
