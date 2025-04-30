06f01c1c10ca7840d04c520d4dc52e880b11b781
Dirk Lemstra <dirk@lemstra.org>
2025-04-30 07:02:47 +0200

    Added extra overload to resize that allows specifying the filter type.

--- tests/magick-image/resize.spec.ts
+++ tests/magick-image/resize.spec.ts
@@ -3,6 +3,7 @@
   Licensed under the Apache License, Version 2.0.
 */
 
+import { FilterType } from '@src/enums/filter-type';
 import { MagickGeometry } from '@src/types/magick-geometry';
 import { TestFiles } from '@test/test-files';
 
@@ -25,9 +26,12 @@ describe('MagickImage#resize', () => {
 
     it('should use the filter type', () => {
         TestFiles.Images.Builtin.logo.use((image) => {
-            image.resize(0, 400);
-            expect(image.width).toBe(533);
-            expect(image.height).toBe(400);
+            image.clone((other) => {
+                image.resize(0, 100);
+                other.resize(0, 100, FilterType.MagicKernelSharp2021);
+
+                expect(image).toEqualImage(other, 0.01742);
+            });
         });
     });
 
@@ -44,6 +48,18 @@ describe('MagickImage#resize', () => {
             image.resize(new MagickGeometry(0, 300));
             expect(image.width).toBe(400);
             expect(image.height).toBe(300);
+        });
+    });
+
+    it('with geometry should use the filter type', () => {
+        TestFiles.Images.Builtin.logo.use((image) => {
+            image.clone((other) => {
+                const geometry = new MagickGeometry(0, 100);
+                image.resize(geometry);
+                other.resize(geometry, FilterType.Jinc);
+
+                expect(image).toEqualImage(other, 0.01177);
+            });
         });
     });
 });
