1df3d9b82950f181c290eb69de00cb819701edfe
Dirk Lemstra <dirk@lemstra.org>
2025-09-08 22:20:27 +0200

    Restored copy of the NOTICE.

--- npm-postinstall.mjs
+++ npm-postinstall.mjs
@@ -15,4 +15,5 @@ function copyFile(src, dest) {
     console.log(`Copied ${src} to ${dest}`);
 };
 
+copyFile('node_modules/@dlemstra/magick-native/NOTICE', 'NOTICE');
 copyFile('node_modules/@dlemstra/magick-native/magick.wasm', 'dist/magick.wasm');
