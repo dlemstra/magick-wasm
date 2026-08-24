61de85da7f237c25a55ab28b6f190cffe57ab0f7
Dirk Lemstra <dirk@lemstra.org>
2026-08-24 21:28:22 +0200

    Updated unit tests due to changes in magick-native.

--- tests/magick/delegates.spec.ts
+++ tests/magick/delegates.spec.ts
@@ -7,6 +7,6 @@ import { Magick } from '@src/magick';
 
 describe('Magick#delegates', () => {
     it('should return the delegates', () => {
-        expect(Magick.delegates).toBe('freetype heic jng jp2 jpeg jxl lcms lqr openexr png raw tiff webp xml zlib');
+        expect(Magick.delegates).toBe('bzlib freetype heic jng jp2 jpeg jxl lcms lqr openexr png raw tiff webp xml zlib');
     });
 });
